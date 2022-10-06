34cc3ed86a9d9d5948cd3edbc55332f8db188065
Mosunov Anton <antonmossunov@gmail.com>
2022-10-06 12:13:53 +0600

    Настройка админ-панели. Регистрация моделей.
    Создан суперпользователь(root-root)
    Настроена панель администратора
    Модели зарегистрированы

--- issue_tracker/tracker/admin.py
+++ issue_tracker/tracker/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from tracker.models import Task, Status, Type
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'description', 'created_at', 'changed_at')
+    list_filter = ('id', 'text', 'description', 'created_at', 'changed_at')
+    search_fields = ('text', 'description', 'created_at', 'updated_at')
+    fields = ('text', 'description')
+    readonly_fields = ('id',)
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'status_name')
+    list_filter = ('id', 'status_name')
+    search_fields = ('status_name',)
+    fields = ('status_name',)
+    readonly_fields = ('id',)
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type_name')
+    list_filter = ('id', 'type_name')
+    search_fields = ('type_name',)
+    fields = ('type_name',)
+    readonly_fields = ('id',)
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Type, TypeAdmin)
