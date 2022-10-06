99f0602471fa878b544ae27919e70cf0fa0ae0d3
Mosunov Anton <antonmossunov@gmail.com>
2022-10-06 14:50:18 +0600

    Создание MVC для страницы детального отображения задачи
    Создана страница task.html
    Настроена маршрутизация
    Настроено классовое представление TaskView для отображениыя детальной страницы
    На главной странице добавлена ссылка дл перехода на детальное отображение задачи

--- issue_tracker/tracker/urls.py
+++ issue_tracker/tracker/urls.py
@@ -2,17 +2,18 @@ from django.contrib import admin
 from django.urls import path
 
 from tracker.views.base import IndexView
-# from webapp.views.articles import ArticleView, ArticleUpdateView, delete_view, confirm_delete, add_view
+from tracker.views.tasks import TaskView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='index'),
-    path('articles/', IndexView.as_view(), name='index'),
+    path('tasks/', IndexView.as_view(), name='index'),
+    path('tasks/<int:pk>', TaskView.as_view(), name='task_detail')
     # path('articles/add/', add_view, name='article_add'),
     # path('articles/<int:pk>/update/', ArticleUpdateView.as_view(), name='article_update'),
     # path('articles/<int:pk>/delete/', delete_view, name='article_delete'),
     # path('articles/<int:pk>/confirm-delete/', confirm_delete, name='confirm_delete'),
 
-    # path('articles/<int:pk>', ArticleView.as_view(), name='article_detail')
+
 ]
