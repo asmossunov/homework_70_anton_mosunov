d1daba02686d159704bd9c3b77133dba8c3a92b4
Mosunov Anton <antonmossunov@gmail.com>
2022-10-06 14:26:57 +0600

    Создание MVC для главной страницы приложения.
    Создан базовый шаблон base.html
    Создана страница index.html
    Создан файл urls.py в приложении tracker
    Настроена маршрутизация
    Создана папка views для реализации представлений
    Настроено классовое представление IndexView для отображениыя главной страницы
    Создана папка static и в ней создан файл стилей
    В файле settings.py прописан путь к статическим файлам - папке static

---
+++ issue_tracker/tracker/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+
+from tracker.views.base import IndexView
+# from webapp.views.articles import ArticleView, ArticleUpdateView, delete_view, confirm_delete, add_view
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
+    path('articles/', IndexView.as_view(), name='index'),
+    # path('articles/add/', add_view, name='article_add'),
+    # path('articles/<int:pk>/update/', ArticleUpdateView.as_view(), name='article_update'),
+    # path('articles/<int:pk>/delete/', delete_view, name='article_delete'),
+    # path('articles/<int:pk>/confirm-delete/', confirm_delete, name='confirm_delete'),
+
+    # path('articles/<int:pk>', ArticleView.as_view(), name='article_detail')
+]
