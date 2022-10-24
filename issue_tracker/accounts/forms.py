0b00b54f22c45b5092feb9ea05568105511d3741
Mosunov Anton <antonmossunov@gmail.com>
2022-10-24 11:57:41 +0600

    Реализация регистрации новых пользователей.
    Создано MVC для регистрации пользовтелей
    Создан щаблон, представление, насторена маршрутизация

--- issue_tracker/accounts/forms.py
+++ issue_tracker/accounts/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class LoginForm(forms.Form):
@@ -6,3 +9,30 @@ class LoginForm(forms.Form):
     password = forms.CharField(required=True, label='Пароль', widget=forms.PasswordInput)
     next = forms.CharField(required=False, widget=forms.HiddenInput)
 
+
+class CustomUserCreationForm(forms.ModelForm):
+    password = forms.CharField(label='Пароль', strip=False, required=True, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label='Подтвердите пароль', strip=False, required=True,
+                                       widget=forms.PasswordInput)
+    username = forms.CharField(label='Имя пользователя', strip=False, required=True)
+    first_name = forms.CharField(label='Имя', strip=False, required=True)
+    last_name = forms.CharField(label='Фамилия', strip=False, required=True)
+    email = forms.EmailField(label='Адрес почты', required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password_confirm', 'first_name', 'last_name', 'email',)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+        if password and password_confirm and password != password_confirm:
+            raise ValidationError('Пароли не совпадают')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get('password'))
+        if commit:
+            user.save()
+            return user
